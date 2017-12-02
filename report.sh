b2eac2a7154be36e37d0bfec7deab7b647cbea5b
datianshi <dsz0111@gmail.com>
2017-12-02 09:06:08 -0600

    add report.sh

---
+++ report.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+for region in `aws ec2 describe-regions --output text | cut -f3`
+do
+  echo -e "Listing EC2 Instances in Region: '$region'\n"
+  aws ec2 describe-instances --output=json --region=${region} | jq -r '[.[][].Instances[] | {InstanceId, LaunchTime: .LaunchTime, Date: .LaunchTime|sub(".[0-9]+Z$"; "Z") | fromdate, AZ: .Placement.AvailabilityZone, KeyName, State: .State.Name}]' | jq 'sort_by(.Date)'
+  echo -e "Listing RDS in Region: $region \n"
+  aws rds describe-db-instances --output=json --region=${region} | jq -r \
+      '.DBInstances | .[] | {InstanceCreateTime, DBInstanceIdentifier, MasterUsername, vpc: .DBSubnetGroup.VpcId}'
+done
