3f74e4a632224fa596428784eac385bd4bc34fb9
datianshi <dsz0111@gmail.com>
2017-12-18 20:38:08 -0500

    add tags to the report

--- report.sh
+++ report.sh
@@ -3,7 +3,7 @@
 for region in `aws ec2 describe-regions --output text | cut -f3`
 do
   echo -e "Listing EC2 Instances in Region: '$region'\n"
-  aws ec2 describe-instances --output=json --region=${region} | jq -r '[.[][].Instances[] | {InstanceId, LaunchTime: .LaunchTime, Date: .LaunchTime|sub(".[0-9]+Z$"; "Z") | fromdate, AZ: .Placement.AvailabilityZone, KeyName, State: .State.Name}]' | jq 'sort_by(.Date)'
+  aws ec2 describe-instances --output=json --region=${region} | jq -r '[.[][].Instances[] | {InstanceType,Tags,InstanceId, LaunchTime: .LaunchTime, Date: .LaunchTime|sub(".[0-9]+Z$"; "Z") | fromdate, AZ: .Placement.AvailabilityZone, KeyName, State: .State.Name}]' | jq 'sort_by(.Date)'
   echo -e "Listing RDS in Region: $region \n"
   aws rds describe-db-instances --output=json --region=${region} | jq -r \
       '.DBInstances | .[] | {InstanceCreateTime, DBInstanceIdentifier, MasterUsername, vpc: .DBSubnetGroup.VpcId}'
